af5b5124c7edb7e4453e97d1c1dbaa13af12221d
hyeon <lhyeonnit@gmail.com>
2024-01-23 11:20:07 +0900

    reflection

---
+++ src/main/java/ex01/UserController.java
@@ -0,0 +1,13 @@
+package ex01;
+
+public class UserController {
+    // /login
+    public void login (){
+        System.out.println("로그인 호출됨");
+    }
+
+    // /join
+    public void join(){
+        System.out.println("회원가입 호출됨");
+    }
+}
